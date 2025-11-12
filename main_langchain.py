c3da4c44079b04f71f5160e88cf88f681d2edefc
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2025-11-12 17:05:18 -0300

    feat: adicionado o PromptTemplate para que eu possa criar o modelo do prompt e depois performá-lo com format

--- main_langchain.py
+++ main_langchain.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
-import os 
+import os # para que eu possa usar as variaveis de ambiente
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate # antes era langchain.prompts, agora é langchain_core.prompts
 
 load_dotenv()
 
@@ -8,7 +9,17 @@ numero_de_dias = 7
 numero_de_criancas = 2
 atividade = "praia"
 
-prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma familia com {numero_de_criancas} crianças, que gostam de {atividade}."
+modelo_do_prompt = PromptTemplate.from_template(
+    "Crie um roteiro de viagem de {dias} dias, para uma familia com {criancas} crianças, que gostam de {atividade}."
+)
+
+prompt = modelo_do_prompt.format(
+    dias=numero_de_dias,
+    criancas=numero_de_criancas,
+    atividade=atividade
+)
+
+print(prompt)
 
 llm = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
